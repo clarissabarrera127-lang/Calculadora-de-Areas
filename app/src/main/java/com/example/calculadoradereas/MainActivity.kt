d7e9bcd9f2316df43f2407991d7fc167c815ea97
clarissabarrera127-lang <clarissabarrera127@gmail.com>
2026-05-17 23:04:17 -0600

    Agregue iconos y sonidos

--- app/src/main/java/com/example/calculadoradereas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradereas/MainActivity.kt
@@ -1,26 +1,45 @@
 package com.example.calculadoradereas
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Enlazamos los botones del XML con el código
-        val btnCuadrado = findViewById<Button>(R.id.btnCuadrado)
-        val btnTriangulo = findViewById<Button>(R.id.btnTriangulo)
-        val btnCirculo = findViewById<Button>(R.id.btnCirculo)
+        // Enlazamos las tarjetas contenedoras (CardViews)
+        val cardCuadrado = findViewById<CardView>(R.id.cardCuadrado)
+        val cardTriangulo = findViewById<CardView>(R.id.cardTriangulo)
+        val cardCirculo = findViewById<CardView>(R.id.cardCirculo)
 
-        // Escuchamos los clics
-        btnCuadrado.setOnClickListener { abrirCalculo("cuadrado") }
-        btnTriangulo.setOnClickListener { abrirCalculo("triangulo") }
-        btnCirculo.setOnClickListener { abrirCalculo("circulo") }
+        // Eventos de clic con sonido y navegación
+        cardCuadrado.setOnClickListener {
+            reproducirSonido(R.raw.cuadrado)
+            abrirCalculo("cuadrado")
+        }
+
+        cardTriangulo.setOnClickListener {
+            reproducirSonido(R.raw.triangulo)
+            abrirCalculo("triangulo")
+        }
+
+        cardCirculo.setOnClickListener {
+            reproducirSonido(R.raw.circulo)
+            abrirCalculo("circulo")
+        }
     }
 
-    // Función para abrir la segunda pantalla enviando un dato ("extra")
+    // Función auxiliar para reproducir cualquier archivo de audio de la carpeta raw
+    private fun reproducirSonido(idSonido: Int) {
+        val mp = MediaPlayer.create(this, idSonido)
+        mp.setOnCompletionListener { it.release() } // Libera la memoria RAM cuando el sonido termina
+        mp.start()
+    }
+
     private fun abrirCalculo(figura: String) {
         val intentActual = Intent(this, CalculoActivity::class.java)
         intentActual.putExtra("FIGURA_SELECCIONADA", figura)
